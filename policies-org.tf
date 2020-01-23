b13f4485e1644f2cd82c48c8bbb91209b06c7e96
ppresto <ppresto@hashicorp.com>
2020-01-22 16:21:26 -0800

    path fix

--- policies-org.tf
+++ policies-org.tf
@@ -18,9 +18,6 @@ resource "tfe_policy_set" "org" {
 
 resource "null_resource" "sentinal_vars" {
 
-  triggers = {
-    shell_hash = "${sha256(file("${path.module}/appsettings.sh"))}"
-  }
   provisioner "local-exec" {
       command = "${path.module}/scripts/create_policyset_vars.sh"
       interpreter = ["sh"]
