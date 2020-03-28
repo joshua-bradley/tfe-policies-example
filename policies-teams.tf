b85cdb191eb24034ef75a2675caa33bf08907bec
ppresto <ppresto@hashicorp.com>
2020-03-27 17:34:27 -0700

    pol8

--- policies-teams.tf
+++ policies-teams.tf
@@ -16,3 +16,24 @@ resource "tfe_policy_set" "teams" {
     oauth_token_id     = "${var.oauth_token_id}"
   }
 }
+
+resource "null_resource" "sentinal_var_teams" {
+  count                  = "${var.policies_org ? 1 : 0}"
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  
+  provisioner "local-exec" {
+      command = "${path.module}/scripts/create_policyset_vars.sh"
+      interpreter = ["bash"]
+      working_dir = "${path.module}/scripts"
+      environment = {
+        ATLAS_TOKEN = "${var.tfe_token}"
+        HOST = "${var.tfe_hostname}"
+        ORGANIZATION = "${var.tfe_organization}"
+      }
+  }
+
+  depends_on = ["tfe_policy_set.teams"]
+}
